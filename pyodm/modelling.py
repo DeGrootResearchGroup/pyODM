55ce1629d567fb9ae8390d0c51e9adf973e31b23
Chris DeGroot <cdegroo5@uwo.ca>
2022-09-01 13:09:28 -0400

    Add a function to generate multi-site spline models

--- pyodm/modelling.py
+++ pyodm/modelling.py
@@ -16,22 +16,35 @@ class AggregateModel():
         for key in self._weights.keys():
             self._weights[key] = self._weights[key]/total_weight
 
-    def get_site_spline(self, site, gene_1, gene_2, start_date, end_date, units="gcL"):
-        """Class to generate a spline curve for a given site"""
-        # Get the spline model for the site
-        model = self._data[site].get_spline_model(gene_1, gene_2, units)
+    def str_to_datetime(self, date):
+        """Function to convert a string of the form YYYY-MM-DD to a datetime object"""
+        return datetime.datetime.strptime(date, "%Y-%m-%d").date()
 
-        # Convert the date strings
-        start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
-        end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+    def get_date_list(self, start_date, end_date):
+        """Function to return a list of dates based on start and end dates"""
+        start_date = self.str_to_datetime(start_date)
+        end_date = self.str_to_datetime(end_date)
 
-        # Generate the lists of dates
         dates = [start_date]
         while dates[-1] <= end_date:
             dates.append(dates[-1] + datetime.timedelta(days=1))
 
-        # Generate the model values
-        values = [model(date) for date in dates]
+        return dates
 
-        # Return a dataframe
+    def get_site_spline(self, site, gene_1, gene_2, start_date, end_date, units="gcL"):
+        """Function to generate a spline curve for a given site"""
+        model = self._data[site].get_spline_model(gene_1, gene_2, units)
+        dates = self.get_date_list(start_date, end_date)
+        values = [float(model(date)) for date in dates]
         return pd.DataFrame({"sampleDate" : dates, "value": values})
+
+    def get_multisite_splines(self, sites, gene_1, gene_2, start_date, end_date, units="gcL"):
+        """Function to generate a weighted spline curve for multiple sites"""
+        splines = self.get_site_spline(sites[0], gene_1, gene_2, start_date, end_date, units)
+        splines = splines.rename(columns={"value" : "value_{}".format(sites[0])})
+        for site in sites[1:]:
+            spline = self.get_site_spline(site, gene_1, gene_2, start_date, end_date, units)
+            splines = splines.merge(spline, how="left", on="sampleDate")
+            splines = splines.rename(columns={"value" : "value_{}".format(site)})
+
+        return splines
