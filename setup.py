277cf412e2f1fc2bfa95239e71f7f1fbaad1b5fc
Chris DeGroot <cdegroo5@uwo.ca>
2022-09-03 01:04:06 -0400

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pyODM',
+    version='0.1dev',
+    packages=['pyodm',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
