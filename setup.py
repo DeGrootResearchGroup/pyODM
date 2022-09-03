544e0c9a17c89d172a3a4ecb74bbc12b7eb1bcf5
Chris DeGroot <cdegroo5@uwo.ca>
2022-09-03 01:08:30 -0400

    Add dependencies

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     packages=['pyodm',],
     license='MIT License',
     long_description=open('README.md').read(),
+    install_requires=['datetime', 'numpy', 'pandas', 'scipy', 'warnings']
 )
