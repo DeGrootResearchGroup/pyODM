8ad50d8194cd5ced4d262734fa351a8c035fa88e
Chris DeGroot <cdegroo5@uwo.ca>
2022-09-03 16:11:45 -0400

    Add submodules to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyODM',
     version='0.1dev',
-    packages=['pyodm',],
+    packages=['pyodm', 'pyodm.odm', 'pyodm.modelling', 'pyodm.sitedata',],
     license='MIT License',
     long_description=open('README.md').read(),
     install_requires=['datetime', 'numpy', 'pandas', 'scipy']
