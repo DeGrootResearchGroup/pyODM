3f6db343d7d6fd0f0c26a2fd8901426b52b51b3a
Chris DeGroot <cdegroo5@uwo.ca>
2022-09-03 16:15:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyODM',
     version='0.1dev',
-    packages=['pyodm', 'pyodm.odm', 'pyodm.modelling', 'pyodm.sitedata',],
+    packages=setuptools.find_packages(),,
     license='MIT License',
     long_description=open('README.md').read(),
     install_requires=['datetime', 'numpy', 'pandas', 'scipy']
